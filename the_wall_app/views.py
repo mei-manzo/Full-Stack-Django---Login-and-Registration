61406b2fdaaed95a1de362da16676fc4ee20762b
Mei Manzo <mmanzo806@gmail.com>
2021-06-15 11:49:36 -0700

    added basic message show

--- the_wall_app/views.py
+++ the_wall_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib import messages
 from .models import *
+from datetime import datetime
+from pytz import timezone
+import pytz
+
 
 def wall(request):
     if 'user_id' not in request.session:
@@ -12,7 +16,17 @@ def wall(request):
     return render(request, "wall.html", context)
 
 def create_message(request):
-    # this_user = User.objects.filter(id = request.session['user_id'])
-    # context = {
-    #     "current_user" : this_user[0] #grabs from session rather than database to prevent refreshing into login
-    # }
+    if request.method == 'POST':
+        #creating date
+        # date_format='%m/%d/%Y %H:%M:%S %Z'
+        # date = datetime.now(tz=pytz.utc)
+        # date = date.astimezone(timezone('US/Pacific'))
+        # myTime = date.strftime(date_format)
+        this_user = User.objects.get(id = request.session['user_id'])
+        new_message = Message.objects.create(user=this_user, message=request.POST['content'])
+        context = {
+            "all_messages": Message.objects.all(),
+            # "all_messages": Message.objects.all()
+        }
+        return render(request, "wall.html", context)
+    return redirect("/wall")
