db5e4afd48c0b6131f222e731b9cb1bcfe39343c
Mei Manzo <mmanzo806@gmail.com>
2021-06-15 19:23:14 -0700

    messages show newest first

--- the_wall_app/models.py
+++ the_wall_app/models.py
@@ -11,14 +11,14 @@ from login_app.models import *
 #     # objects = UserManager()
 
 class Message(models.Model):
-    user = models.ForeignKey(User, related_name="messages", on_delete = models.CASCADE)
+    poster = models.ForeignKey(User, related_name="messages", on_delete = models.CASCADE)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now=True)
     
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, related_name="comments", on_delete = models.CASCADE)
+    poster = models.ForeignKey(User, related_name="comments", on_delete = models.CASCADE)
     message = models.ForeignKey(Message, related_name="comments", on_delete = models.CASCADE)
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add = True)
--- the_wall_app/views.py
+++ the_wall_app/views.py
@@ -3,30 +3,30 @@ from django.contrib import messages
 from .models import *
 from datetime import datetime
 from pytz import timezone
+from login_app import views
 import pytz
 
+# def wall_success(request):
+#     if 'user_id' not in request.session:
+#         return redirect('/')
+#     # this_user = User.objects.filter(id = request.session['user_id'])
+#     context = {
+#         'all_messages': Message.objects.all()
+#     }
+#     return render(request, "success.html", context)
 
 def wall(request):
     if 'user_id' not in request.session:
         return redirect('/')
     this_user = User.objects.filter(id = request.session['user_id'])
     context = {
-        "current_user" : this_user[0] #grabs from session rather than database to prevent refreshing into login
+        "current_user" : this_user[0], #grabs from session rather than database to prevent refreshing into login
+        "all_messages": Message.objects.all(),
+        "newest_first": Message.objects.all().order_by("-updated_at")
     }
     return render(request, "wall.html", context)
 
 def create_message(request):
-    if request.method == 'POST':
-        #creating date
-        # date_format='%m/%d/%Y %H:%M:%S %Z'
-        # date = datetime.now(tz=pytz.utc)
-        # date = date.astimezone(timezone('US/Pacific'))
-        # myTime = date.strftime(date_format)
-        this_user = User.objects.get(id = request.session['user_id'])
-        new_message = Message.objects.create(user=this_user, message=request.POST['content'])
-        context = {
-            "all_messages": Message.objects.all(),
-            # "all_messages": Message.objects.all()
-        }
-        return render(request, "wall.html", context)
-    return redirect("/wall")
+    Message.objects.create(poster=User.objects.get(id=request.session['user_id']), message=request.POST['content'])
+    return redirect('/wall')
+
